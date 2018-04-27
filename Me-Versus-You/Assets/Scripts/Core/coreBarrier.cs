141ab91caba8c33093ae8a0ce373a9e15bb6f533
Edward S Vo <edwardsavo@gmail.com>
2018-04-27 02:07:52 -0700

    BOSS UPDATE

---
+++ Me-Versus-You/Assets/Scripts/Core/coreBarrier.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coreBarrier : MonoBehaviour {
+   
+    private float coreHealth = 1000f;
+
+    public celion_Player celion;
+    public GameObject otherObjects;
+
+
+    void Start ()
+    {
+
+        celion = otherObjects.GetComponent<celion_Player>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, 0, 110 * Time.deltaTime);
+		
+	}
+    public void Damage(float Damage)
+    {
+        coreHealth -= Damage;
+
+        if (coreHealth < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void soundDamage(string sound)
+    {
+        if (sound == "bulletHit" || sound == "starHit")
+        {
+            celion.audioManager.PlaySound("barrierGuard");
+        }
+    }
+}
